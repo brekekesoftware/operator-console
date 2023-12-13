1af899071d786294ecca7599aa1799c26b789b47
dai <dai2pon@gmail.com>
2023-12-13 11:33:57 +0900

    UC chat agent component(Uccac)関連のファイルの追加

---
+++ dist/uccac/js/UccacUtil.js
@@ -0,0 +1,12 @@
+class UccacUtil {
+    static findElementByAttributeNameToParent( eChild, attrName ){
+        let e;
+        while( ( e = eChild.parentElement ) != null ) {
+            const b = e.hasAttribute( attrName );
+            if( b ){
+                return e;
+            }
+            eChild = e;
+        }
+    }
+}
